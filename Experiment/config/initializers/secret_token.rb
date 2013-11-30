8ddb563757a0f18b4b72314536c58ead2520e23e
Huu Nguyen <hknguyen13@gmail.com>
2013-11-29 23:10:44 -0500

    init commit

---
+++ Experiment/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Experiment::Application.config.secret_key_base = '28bf65fecbed16def3487d0b67812299a70467d20bd03f3e8bf9671c632e93bedce83de7d7bdb4cac356efd337d08dcc73d5e16e04b779fff88783df9413ce81'
---
+++ Experiment/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Experiment::Application.config.session_store :cookie_store, key: '_Experiment_session'
